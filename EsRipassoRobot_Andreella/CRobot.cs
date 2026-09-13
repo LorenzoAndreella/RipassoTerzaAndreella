24471523c41d9789d3dd08895bcfab1a784cbee2
LorenzoAndreella <14529876@itisrossi.vi.it>
2026-09-13 14:29:34 +0200

    caricamento

---
+++ EsRipassoRobot_Andreella/CRobot.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EsRipassoRobot_Andreella
+{
+    internal class CRobot
+    {
+        private string modello;
+        private float potenza;
+        private int stato;
+        private int anno;
+
+        public string Modello
+        {
+            get { return modello; }
+        }
+        public float Potenza
+        {
+            get { return potenza; }
+        }
+        public int Stato
+        {
+            get { return stato; }
+        }
+        public int Anno
+        {
+            get { return anno; }
+        }
+
+        public CRobot(string modello, float potenza, int stato, int anno)
+        {
+            this.modello = modello;
+            this.potenza = potenza;
+            this.stato = stato;
+            this.anno = anno;
+        }
+
+        public string Visualizza()
+        {
+            string statoDescrizione = stato > 0 ? "Operativo" : "Guasto";
+            return $"Modello: {Modello}\tPotenza: {potenza}kW\tStato: {statoDescrizione}\tAnno: {anno}";
+        }
+    }
+
+}
---
+++ EsRipassoRobot_Andreella/Program.cs
@@ -0,0 +1,141 @@
+﻿using EsRipassoRobot_Andreella;
+using System.ComponentModel.Design;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Inserimento Robot");
+        List<CRobot> listaRobot = InserisciLista();
+
+        Console.WriteLine("Visualizza lista");
+        VisualizzaLista(listaRobot);
+
+        Console.WriteLine("Filtra per potenza");
+        FiltraPotenza(listaRobot);
+
+        Console.WriteLine("Visualizza per anno");
+        CRobot robotPiùRecente = OrdinaAnno(listaRobot);
+        Console.WriteLine("Robot più recente");
+        Console.WriteLine(robotPiùRecente.Visualizza());
+
+    }
+
+    public static List<CRobot> InserisciLista()
+    {
+        List<CRobot> ListaRobot = new List<CRobot>();
+        int n;
+        do
+        {
+            Console.WriteLine("Quanti robot vuoi inserire? (max 100)");
+        }
+        while (!int.TryParse(Console.ReadLine(), out n) || n < 1 || n > 100);
+
+        for (int i = 0; i < n; i++)
+        {
+            string modello;
+            float potenza;
+            int stato;
+            int anno;
+            Console.WriteLine("Robot numero " + (i + 1) + ":");
+            do
+            {
+                Console.WriteLine("Modello del robot:");
+                modello = Console.ReadLine();
+            } while (string.IsNullOrWhiteSpace(modello));
+
+            do
+            {
+                Console.WriteLine("Potenza del robot:");
+
+            } while (!float.TryParse(Console.ReadLine(), out potenza) || potenza < 0);
+
+            do
+            {
+                Console.WriteLine("Stato del robot (0 guasto; >0 Operativo) :");
+            } while (!int.TryParse(Console.ReadLine(), out stato) || stato < 0);
+
+            do
+            {
+                Console.WriteLine("Anno del robot:");
+            } while (!int.TryParse(Console.ReadLine(), out anno) || anno < 1950 || anno > DateTime.Now.Year);
+
+            CRobot robot = new CRobot(modello, potenza, stato, anno);
+            ListaRobot.Add(robot);
+        }
+
+        return ListaRobot;
+
+    }
+
+    public static void VisualizzaLista(List<CRobot> ListaRobot)
+    {
+        bool trovato = false;
+        foreach (CRobot robot in ListaRobot)
+        {
+            if (robot.Stato > 0)
+            {
+                Console.WriteLine(robot.Visualizza());
+                trovato = true;
+            }
+
+            
+        }
+        if (trovato == false)
+        {
+            Console.WriteLine("Nessun robot operativo trovato.");
+        }
+
+    }
+
+    public static void FiltraPotenza(List<CRobot> lista)
+    {
+        float pot;
+        do
+        {
+            Console.WriteLine("Inserisci potenza massima dei robot da visualizzare");
+        } while (!float.TryParse(Console.ReadLine(), out pot) || pot < 0);
+
+        bool trovato = false;
+
+        foreach (CRobot robot in lista)
+        {
+            if (robot.Potenza <= pot)
+            {
+                Console.WriteLine(robot.Visualizza());
+                trovato = true;
+            }
+
+        }
+
+        if (trovato == false)
+        {
+            Console.WriteLine("Nessun robot con potenza minore di " + pot + " trovato");
+        }
+    }
+
+    public static CRobot OrdinaAnno(List<CRobot> lista)
+    {
+        
+        int n = lista.Count;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - 1 - i; j++)
+            {
+                if (lista[j].Anno > lista[j + 1].Anno)
+                {
+                    CRobot temp = lista[j];
+                    lista[j] = lista[j + 1];
+                    lista[j+1] = temp;
+                }
+            }
+        }
+
+        foreach (CRobot robot in lista)
+        {
+            Console.WriteLine(robot.Visualizza());
+        }
+
+        return lista[n-1];
+    }
+}
